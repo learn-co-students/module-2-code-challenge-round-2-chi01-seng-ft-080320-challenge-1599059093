18ab9acc28c4b514340b3b975e3a5c1f81140fbb
Nick Moore <40217217+NMoore42@users.noreply.github.com>
2020-09-02 13:19:18 -0500

    Revert "Natalia"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests
-  resources :episodes
-  resources :appearances
-
+  resources :guests, only: [:index]
+  resources :episodes, only: [:index]
 end
